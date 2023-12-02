10f2eccd6cd6acfaf36df39a5ee5916119d83e49
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-12-02 16:28:36 -0300

    removendo codigo nao utilizado

--- lib/model/livro_list_model.dart
+++ lib/model/livro_list_model.dart
@@ -1,54 +1,24 @@
 import 'package:aplicativo_leitor_de_ebooks/model/livro_model.dart';
 
 class LivroListModel {
-  final List<dynamic> livros;
+  final List<LivroModel> livros;
 
   const LivroListModel({
     required this.livros,
   });
 
   factory LivroListModel.fromJson(List<dynamic> parsedJson) {
-    return LivroListModel(
-      livros: parsedJson,
-    );
-  }
-
-  factory LivroListModel.fromJson2(List<dynamic> parsedJson) {
     List<LivroModel> livros = [];
     livros = parsedJson
-        .map((e) => LivroModel(
-              id: e["id"],
-              title: e["title"],
-              author: e["author"],
-              coverUrl: e["coverUrl"],
-              downloadUrl: e["downloadUrl"],
+        .map((item) => LivroModel(
+              id: item["id"],
+              title: item["title"],
+              author: item["author"],
+              coverUrl: item["cover_url"],
+              downloadUrl: item["download_url"],
             ))
         .toList();
     return LivroListModel(
-      livros: livros,
-    );
-  }
-}
-
-class LivroListModel2 {
-  final List<LivroModel> livros;
-
-  const LivroListModel2({
-    required this.livros,
-  });
-
-  factory LivroListModel2.fromJson(List<dynamic> parsedJson) {
-    List<LivroModel> livros = [];
-    livros = parsedJson
-        .map((e) => LivroModel(
-              id: e["id"],
-              title: e["title"],
-              author: e["author"],
-              coverUrl: e["cover_url"],
-              downloadUrl: e["download_url"],
-            ))
-        .toList();
-    return LivroListModel2(
       livros: livros,
     );
   }
